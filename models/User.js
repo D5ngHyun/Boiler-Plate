58709d47ef1df435464eba3dbc08115bc5a5e68d
Dongs <dongs@Dongsui-MacBookAir.local>
2022-05-18 19:02:19 +0900

    library bcrypt ADD

--- models/User.js
+++ models/User.js
@@ -1,5 +1,9 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 const { Schema } = mongoose;
+const saltRounds = 10;
+// const myPlaintextPassword = "s0//P4$$w0rD";
+// const someOtherPlaintextPassword = "not_bacon";
 
 const userSchema = new Schema({
   name: {
@@ -31,6 +35,26 @@ const userSchema = new Schema({
   image: String,
   token: String,
   tokenExp: Number,
+});
+
+userSchema.pre("save", function (next) {
+  const user = this;
+
+  // 비밀번호를 암호화
+  bcrypt.genSalt(saltRounds, function (err, salt) {
+    if (err) return next(err);
+
+    if (user.isModified("password")) {
+      bcrypt.hash(user.password, salt, function (err, hash) {
+        // Store hash in your password DB.
+        if (err) return next(err);
+
+        user.password = hash;
+
+        next();
+      });
+    }
+  });
 });
 
 const User = mongoose.model("User", userSchema);
