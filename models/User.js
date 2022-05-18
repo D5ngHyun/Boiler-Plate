2a0c3afa276d7840bcfbf4ba29b4d1d10d465f25
Dongs <dongs@Dongsui-MacBookAir.local>
2022-05-18 17:14:11 +0900

    model && Schema Add

---
+++ models/User.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const userSchema = new Shema({
+  name: {
+    type: String,
+    maxLength: 50,
+  },
+
+  email: {
+    type: String,
+    trim: true,
+    unique: 1,
+  },
+
+  password: {
+    type: String,
+    minLengh: 5,
+  },
+
+  lastName: {
+    type: String,
+    maxLength: 50,
+  },
+
+  role: {
+    type: Number,
+    default: 0,
+  },
+
+  image: String,
+  token: String,
+  tokenExp: Number,
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
